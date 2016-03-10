26f20b9c806b10887ce102b1dd0f61353902f97d
doriswu17 <wuyuting@umich.edu>
2016-03-09 23:01:01 -0500

    commit
    commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,28 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', function($scope) {
+	$scope.sum=0;
+	$scope.fibnum = 1;
+	$scope.fibbefore = 0;
+	$scope.firsttime = true;
+	$scope.add=function(){
+		$scope.sum = $scope.fibnum + $scope.fibbefore;
+		if($scope.sum == 1 && $scope.firsttime){
+			$scope.fibbefore = 0;
+			$scope.firsttime = false;
+		}
+		else{
+			$scope.fibbefore = $scope.fibnum;
+		}
+		$scope.fibnum = $scope.sum;
+	}
 
-}]);
+	$scope.reset=function(){
+		$scope.fibnum = 1;
+		$scope.fibbefore= 0;
+		$scope.sum = 0;
+		$scope.firsttime = true;
+
+	}
+});
